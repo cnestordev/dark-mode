5b36d025768d548a7c7fa8e19376c2e232d505ec
Nestor <ncervan87@gmail.com>
2020-08-05 15:29:08 -0700

    minor refactoring

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,14 @@
-import React, { useState } from 'react'
-
+import React from 'react'
 
 const Navbar = (props) => {
-
 
   return (
     <nav className="navbar">
       <h1>Crypto Tracker</h1>
-      <div className="dark-mode__toggle">
-        <div
-          onClick={() => props.toggler()}
-          className={props.darkmode ? 'toggle toggled' : 'toggle'}
-        />
+      <div onClick={() => props.toggler()} className="dark-mode__toggle">
+        <div className={props.darkmode ? 'toggle toggled' : 'toggle'} />
       </div>
     </nav>
-  );
-};
-// test.............................
-export default Navbar;
+  )
+}
+export default Navbar
