ab61088f50f0731bdb0bbd09e27f7224086735dc
Nestor <ncervan87@gmail.com>
2020-08-05 14:23:28 -0700

    add custom hooks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
-const Navbar = () => {
-  const [darkMode, setDarkMode] = useState(false);
-  const toggleMode = e => {
-    e.preventDefault();
-    setDarkMode(!darkMode);
-  };
+
+const Navbar = (props) => {
+
+
   return (
     <nav className="navbar">
       <h1>Crypto Tracker</h1>
       <div className="dark-mode__toggle">
         <div
-          onClick={toggleMode}
-          className={darkMode ? 'toggle toggled' : 'toggle'}
+          onClick={() => props.toggler()}
+          className={props.darkmode ? 'toggle toggled' : 'toggle'}
         />
       </div>
     </nav>
   );
 };
-
+// test.............................
 export default Navbar;
